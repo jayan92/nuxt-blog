daf32c089269daab3231223d91852ab052c0ff47
Jayan <mail4jayanchinthaka@gmail.com>
2023-01-26 15:52:01 +0530

    initial commit

---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" @submit="onSubmitted"/>
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from'@/components/Admin/AdminPostForm'
+
+export default {
+  layout: 'admin',
+  components: {
+    AdminPostForm
+  }, 
+  asyncData(context) {
+    return this.$axios
+      .$get(
+        "/posts/" +
+          context.params.postId +
+          ".json"
+      )
+      .then(res => {
+        return {
+          loadedPost: { ...res.data, id: context.params.postId }
+        };
+      })
+      .catch(e => context.error());
+  },
+  methods: {
+    onSubmitted(editedPost) {
+      this.$store.dispatch("editPost", editedPost).then(() => {
+        this.$router.push("/admin");
+      });
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .update-form {
+    width: 90% !important;
+    margin: 20px auto;
+  }
+
+  @media (min-width: 768px) {
+    .update-form {
+      width: 500px !important;
+    }
+  }
+</style>
---
+++ pages/admin/new-post/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="admin-new-post-page">
+    <section class="new-post-form">
+      <AdminPostForm @submit="onSubmitted"/>
+    </section>
+  </div>
+</template>
+
+<script>
+import axios from'axios'
+import AdminPostForm from'@/components/Admin/AdminPostForm'
+
+export default {
+  layout: 'admin',
+  components: {
+    AdminPostForm
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+    onSubmitted(postData) {
+     this.$store.dispatch('addPost', postData).then(() => {
+      this.$router.push('/admin');
+     });
+    }
+  },
+}
+</script>
+
+<style scoped>
+  .new-post-form {
+    width: 90% !important;
+    margin: 20px auto;
+  }
+
+  @media (min-width: 768px) {
+    .new-post-form {
+      width: 500px !important;
+    }
+  }
+</style>
